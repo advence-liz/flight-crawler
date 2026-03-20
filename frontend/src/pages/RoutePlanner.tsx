8add9e733a462de43d153814e94f746c4a7cdaa2
lizhuo13 <lizhuo13@meituan.com>
2026-03-20 22:46:53 +0800

    fix(RoutePlanner): 出发地/目的地选择器再缩减 50% (md=2)

--- frontend/src/pages/RoutePlanner.tsx
+++ frontend/src/pages/RoutePlanner.tsx
@@ -594,7 +594,7 @@ function PlanTab({ cities, urlParams, dateRange }: PlanTabProps) {
           initialValues={{ maxTransfers: 1, flightType: '2666权益卡航班' }}
         >
           <Row gutter={[16, 16]}>
-            <Col xs={12} sm={6} md={3}>
+            <Col xs={12} sm={4} md={2}>
               <Form.Item name="origin" rules={[{ required: true, message: '请选择出发地' }]}>
                 <Select
                   placeholder="出发地"
@@ -608,7 +608,7 @@ function PlanTab({ cities, urlParams, dateRange }: PlanTabProps) {
               </Form.Item>
             </Col>
 
-            <Col xs={12} sm={6} md={3}>
+            <Col xs={12} sm={4} md={2}>
               <Form.Item name="destination" rules={[{ required: true, message: '请选择目的地' }]}>
                 <Select
                   placeholder="目的地"
