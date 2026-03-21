e916ba77ff454b9a4d8530cafed9cc7530629784
lizhuo13 <lizhuo13@meituan.com>
2026-03-21 18:27:06 +0800

    fix(RoutePlanner): 中转/权益卡 md=6，布局填满 24 格

--- frontend/src/pages/RoutePlanner.tsx
+++ frontend/src/pages/RoutePlanner.tsx
@@ -645,7 +645,7 @@ function PlanTab({ cities, urlParams, dateRange }: PlanTabProps) {
               </Form.Item>
             </Col>
 
-            <Col xs={16} sm={8} md={5}>
+            <Col xs={16} sm={8} md={6}>
               <Form.Item label="中转/权益卡" style={{ marginBottom: 0 }}>
                 <Space.Compact style={{ width: '100%' }}>
                   <Form.Item name="maxTransfers" noStyle>
